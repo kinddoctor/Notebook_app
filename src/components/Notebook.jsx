149abfad0c94de874d3e13a75d7d47a41808fbc7
kinddoctor <tata-yagodina@yandex.ru>
2024-10-13 11:27:58 +0500

    change colors

--- src/components/Notebook.jsx
+++ src/components/Notebook.jsx
@@ -4,7 +4,7 @@ function Notebook() {
       <div className="h-50">
         <button type="button" className="btn btn-primary btn-lg mt-3">Загрузить записи</button>
       </div>
-      <div className="h-100 w-50 p-3 border border-primary rounded me-2 ms-4">
+      <div className="h-100 w-50 p-3 border border-primary rounded shadow-sm me-2 ms-4">
         <ul>
           <li>Запись для примера</li>
           <li>Какая-то инфа</li>
