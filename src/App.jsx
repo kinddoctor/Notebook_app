31f3fa6019e02a35d0ad19f5a8374b392c648ce6
kinddoctor <tata-yagodina@yandex.ru>
2024-10-13 11:12:15 +0500

    add AddingForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Notebook from './components/Notebook.jsx';
+import AddingForm from './components/AddingForm.jsx';
 
 function App() {
   return (
     <div className="d-flex flex-column justify-content-center align-content-center h-100">
       <Notebook />
+      <AddingForm />
     </div>
   )
 }
---
+++ src/components/AddingForm.jsx
@@ -0,0 +1,29 @@
+import { Formik, Form, Field } from 'formik';
+
+function AddingForm () {
+  return (
+    <div className="align-self-center d-flex flex-column justify-content-center align-items-center w-75 p-4 m-4">
+      <p>
+        Поле для добавления заметки
+      </p>
+      <Formik
+      initialValues={{ title: '', body: '' }}
+      onSubmit={(values) => {
+        g
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className="d-flex flex-column justify-content-center w-75">
+          <Field type="title" name="title" className="form-control mb-3" placeholder="Заголовок" />
+          <Field type="body" name="body" className="form-control mb-3" placeholder="Текст заметки" />
+          <button type="submit" disabled={isSubmitting} className="btn btn-secondary btn-lg mt-2">
+            Добавить заметку в блокнот
+          </button>
+        </Form>
+      )}
+    </Formik>
+    </div>
+  )
+}
+
+export default AddingForm;
