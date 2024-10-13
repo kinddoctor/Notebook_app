b2ae932d333595431dc08bb26f7d202ea34e7a6e
kinddoctor <tata-yagodina@yandex.ru>
2024-10-13 11:13:44 +0500

    fix adaptivity of Notebook component

--- src/components/Notebook.jsx
+++ src/components/Notebook.jsx
@@ -1,10 +1,10 @@
 function Notebook() {
   return (
-    <div className="d-flex justify-content-center w-100">
+    <div className="d-flex justify-content-center w-100 p-3">
       <div className="h-50">
         <button type="button" className="btn btn-primary btn-lg mt-3">Загрузить записи</button>
       </div>
-      <div className="h-100 w-50 p-3 border border-primary rounded mx-5">
+      <div className="h-100 w-50 p-3 border border-primary rounded me-2 ms-4">
         <ul>
           <li>Запись для примера</li>
           <li>Какая-то инфа</li>
